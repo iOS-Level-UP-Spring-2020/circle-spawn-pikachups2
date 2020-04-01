a9e37e8174de673226ed4885c31341d19ffaac22
Krystian Duma <git@krystian.duma.sh>
2020-04-01 17:36:24 +0200

    Triple Tap Handler - Circles Creation

--- CircleSpawn/CircleSpawn/CircleSpawnController.swift
+++ CircleSpawn/CircleSpawn/CircleSpawnController.swift
@@ -8,4 +8,34 @@ class CircleSpawnController: UIViewController {
 		view = UIView()
 		view.backgroundColor = .white
 	}
+    private var circles: [UIView] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(handleTripleTap(_:)))
+        tripleTap.numberOfTapsRequired = 3
+        view.addGestureRecognizer(tripleTap)
+    }
+
+    @objc func handleTripleTap(_ tap: UITapGestureRecognizer) {
+        let size: CGFloat = 100
+        let spawnedView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: size, height: size)))
+        spawnedView.center = tap.location(in: view)
+        spawnedView.backgroundColor = UIColor.randomBrightColor()
+        spawnedView.layer.cornerRadius = size * 0.5
+        view.addSubview(spawnedView)
+        circles.append(spawnedView)
+        
+        spawnedView.alpha = 0
+        spawnedView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        UIView.animate(withDuration: 0.2, animations: {
+            spawnedView.alpha = 1
+            spawnedView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }, completion: { completed in
+            UIView.animate(withDuration: 0.1, animations: {
+                spawnedView.transform = .identity
+            })
+        })
+    }
 }
