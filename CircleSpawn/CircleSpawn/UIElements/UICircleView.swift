0c060908819ea2275700d6bd74ef51522bdb1058
botichelli <regmesmal@gmail.com>
2020-04-03 20:46:44 +0200

    Moving circles (but wrong gesture recognizer)

--- CircleSpawn/CircleSpawn/UIElements/UICircleView.swift
+++ CircleSpawn/CircleSpawn/UIElements/UICircleView.swift
@@ -16,6 +16,9 @@ class UICircleView: UIView {
         self.center = center
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = size * 0.5
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        self.addGestureRecognizer(pan)
     }
 
     func easeIn() {
@@ -36,6 +39,39 @@ class UICircleView: UIView {
             self.alpha = 0
             self.transform = CGAffineTransform(scaleX: 2, y: 2)
         }, completion: completion)
+    }
+    
+    func onSelect() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.alpha = 0.5
+            self.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        })
+    }
+    
+    func onSelectDismissed() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.alpha = 1
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        })
+    }
+    
+    @objc func handlePan(_ pan: UIPanGestureRecognizer) {
+        guard  let view = pan.view else { return }
+        let circle = view as! UICircleView
+        let translation = pan.translation(in: view)
+        
+        switch pan.state {
+        case .began:
+            circle.onSelect()
+        case .changed:
+            circle.center.x += translation.x
+            circle.center.y += translation.y
+            pan.setTranslation(.zero, in: circle)
+        case .ended, .cancelled:
+            circle.onSelectDismissed()
+        default:
+            return
+        }
     }
 
     required init?(coder: NSCoder) {
